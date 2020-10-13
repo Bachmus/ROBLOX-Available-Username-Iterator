bddfc1f4fd6f5e6f92919e255bfc2888081cdf14
Bachmus <72765561+Bachmus@users.noreply.github.com>
2020-10-13 04:39:44 +0300

    API implementation for username filtering query.
    Makes a second query to the API & checks if the username is filtered by ROBLOX.
    This still has some exceptions.

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import itertools
 import threading
 import requests
 import logging
-import json
-import time
 
 logging.basicConfig(filename='non_existing_usernames.log', level=logging.INFO)
 
@@ -21,21 +19,46 @@ headers = {
 }
 
 
+def filtered_user(username):
+    url = 'https://users.roblox.com/v1/users/search?keyword=%s&limit=10' % username
+    urlHandler = requests.get(url, headers=headers)
+    if urlHandler.status_code == 200:
+        # Either an exception username or it simply does not exist yet.
+        print("Username: %s does not exist yet and has been logged to file. Exceptions do exist though." % username)
+        logging.info(
+            "Username: %s does not exist yet. NOTE: There are exceptions! Roblox's API does NOT always find certain taken usernames." % username)
+    elif urlHandler.status_code == 400:
+        # Bad username. Filtered by Roblox.
+        print(
+            "Username query has been filtered by ROBLOX's filtering system: %s" % username)
+    elif urlHandler.status_code == 429:
+        # Too many requests.
+        print("API reported: %s Too many requests for username: %s" %
+              (urlHandler.status_code, username))
+    else:
+        # Unexpected error.
+        print("FILTERING Response failed with code: %s RETRYING..." %
+              urlHandler.status_code)
+        return filtered_user(username)
+
+
 def fetch_user(username):
     data = '{"usernames": ["%s"], "excludeBannedUsers": false}' % username
     # User API url. See all: https://users.roblox.com/docs
-    url = 'https://users.roblox.com/v1/usernames/users'
-    urlHandler = requests.post(url, headers=headers, data=data)
+    URL = 'https://users.roblox.com/v1/usernames/users'
+    urlHandler = requests.post(URL, headers=headers, data=data)
     if urlHandler.status_code == 200:
         root_doc = urlHandler.json()
         if root_doc['data']:
-            print("uid: ", root_doc['data'][0]['id'], ", Username: ", username)
+            print("uid: %s, Username: %s" %
+                  (root_doc['data'][0]['id'], username))
         else:
-            logging.info('Username: %s does not exist yet.' % username)
+            # Is this username filtered by ROBLOX or genuinely not taken? (NOTE! Some taken username exceptions DO exist, ex. "a0uq")
+            print("--------------------------------------------")
+            filtered_user(username)
         return
     elif urlHandler.status_code == 400:
-        print("Filtering system has blocked the following username: ",
-              urlHandler.status_code, " for user: ", username)
+        print('%s error code.' % urlHandler.status_code)
     else:
         print('REQUEST FAILED FOR: %s RETRYING...' % username)
         # Retry finding user if request fails. This is normal with larger bulk requests.
